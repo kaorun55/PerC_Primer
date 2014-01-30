60a5d270fa7a1f6b935e26ea4bb54d09076540c1
Kaoru NAKAMURA <kaorun55@gmail.com>
2014-01-30 12:31:57 +0900

    Depthカメラの表示を原稿に合わせた

--- samples/04_DepthCamera/04_DepthCamera/04_DepthCamera.cpp
+++ samples/04_DepthCamera/04_DepthCamera/04_DepthCamera.cpp
@@ -5,22 +5,39 @@
 
 #include <iostream>
 
+// ヘッダーファイル
 #include "util_pipeline.h"
 #include <opencv2\opencv.hpp>
 
+// UtilPipelineから継承したクラス
 class Pipeline: public UtilPipeline {
+protected:
+
+  // 1. 定数、変数の宣言 
+  //static const int Width = 640;
+  //static const int Height = 480;
+  static const int Width = 1280;
+  static const int Height = 720;
+
+  // Depthカメラの解像度
+  static const int DEPTH_WIDTH = 320;   // 追加
+  static const int DEPTH_HEIGHT = 240;  // 追加
+
+  PXCImage::ColorFormat colorFormat;
+
 public:
 
+  // 2. コンストラクター
   Pipeline(void)
     : UtilPipeline()
     , colorFormat( PXCImage::COLOR_FORMAT_RGB32 )
   {
     // 必要なデータを有効にする
-    EnableGesture();
     EnableImage( colorFormat, Width, Height);
+    EnableImage( PXCImage::COLOR_FORMAT_DEPTH, DEPTH_WIDTH, DEPTH_HEIGHT);
   }
 
-  // 新しいフレーム
+  // 3. 新しいフレームの更新イベント
   virtual bool OnNewFrame(void)
   {
     try {
@@ -28,15 +45,12 @@ public:
       auto colorFrame = QueryImage( PXCImage::IMAGE_TYPE_COLOR );
       auto depthFrame = QueryImage( PXCImage::IMAGE_TYPE_DEPTH );
 
-      // 取得したデータを表示する
+      // フレームデータを取得する
       cv::Mat colorImage( Height, Width, CV_8UC4 );
-      cv::Mat depthImage( DEPTH_HEIGHT, DEPTH_WIDTH, CV_8U );
-      showColorFrame( colorImage, colorFrame );
-      showDepthFrame( depthImage, depthFrame );
+      cv::Mat depthImage;
+      getColorData( colorImage, colorFrame );
+      getDepthData( depthImage, depthFrame );
       
-      // 画像を左右反転する
-      //cv::flip( colorImage, colorImage, 1 );
-
       // 表示
       cv::imshow( "Color Camera", colorImage );
       cv::imshow( "Depth Camera", depthImage );
@@ -49,37 +63,44 @@ public:
     return key != 'q';
   }
 
-  // Colorデータを表示する
-  void showColorFrame( cv::Mat& colorImage, PXCImage* colorFrame )
+  // Colorデータを取得する
+  void getColorData( cv::Mat& colorImage, PXCImage* colorFrame )
   {
     // Colorデータを取得する
     PXCImage::ImageData data = { 0 };
-    auto sts = colorFrame->AcquireAccess( PXCImage::ACCESS_READ, colorFormat, &data );
+    auto sts = colorFrame->AcquireAccess( PXCImage::ACCESS_READ,
+                                          colorFormat, &data );
     if ( sts < PXC_STATUS_NO_ERROR ) {
       return;
     }
 
-    // Colorデータを取得する
+    // Colorデータを可視化する
     memcpy( colorImage.data, data.planes[0], data.pitches[0] * Height );
 
     // Colorデータを解放する
     colorFrame->ReleaseAccess( &data );
   }
 
-  // Depthデータを表示する
-  void showDepthFrame( cv::Mat& depthImage, PXCImage* depthFrame )
+  // 4. Depthカメラのデータを取得する
+  void getDepthData( cv::Mat& depthImage, PXCImage* depthFrame )
   {
+#if 0
     // Depthデータを取得する
     PXCImage::ImageData data = { 0 };
-    auto sts = depthFrame->AcquireAccess( PXCImage::ACCESS_READ, PXCImage::COLOR_FORMAT_DEPTH, &data );
+    auto sts = depthFrame->AcquireAccess( PXCImage::ACCESS_READ,
+                                          PXCImage::COLOR_FORMAT_DEPTH, &data );
     if ( sts < PXC_STATUS_NO_ERROR ) {
       return;
     }
 
+    // 8bitのGray画像を作成する
+    depthImage = cv::Mat( DEPTH_HEIGHT, DEPTH_WIDTH, CV_8U );
+
     // Depthデータを可視化する
     ushort* srcDepth = (ushort*)data.planes[0];
     uchar* dstDepth = (uchar*)depthImage.data;
-    for ( int i = 0; i < DEPTH_WIDTH * DEPTH_HEIGHT; ++i ) {
+    for ( int i = 0; i < (DEPTH_WIDTH * DEPTH_HEIGHT); ++i ) {
+      // 一定の距離のみ有効にする
       if ( (150 <= srcDepth[i]) && (srcDepth[i] < 800) ) {
         dstDepth[i] = srcDepth[i] * 0xFF / 1000;
       }
@@ -90,19 +111,25 @@ public:
 
     // Depthデータを解放する
     depthFrame->ReleaseAccess( &data );
+#else
+    // Depthデータを取得する
+    PXCImage::ImageData data = { 0 };
+    auto sts = depthFrame->AcquireAccess( PXCImage::ACCESS_READ,
+                                          PXCImage::COLOR_FORMAT_RGB32, &data );
+    if ( sts < PXC_STATUS_NO_ERROR ) {
+      return;
+    }
+
+    // 32bitのRGB画像を作成する
+    depthImage = cv::Mat( DEPTH_HEIGHT, DEPTH_WIDTH, CV_8UC4 );
+
+    // Depthデータを可視化する
+    memcpy( depthImage.data, data.planes[0], data.pitches[0] * DEPTH_HEIGHT );
+
+    // Depthデータを解放する
+    depthFrame->ReleaseAccess( &data );
+#endif
   }
-
-protected:
-
-  static const int Width = 640;
-  static const int Height = 480;
-  //static const int Width = 1280;
-  //static const int Height = 720;
-
-  static const int DEPTH_WIDTH = 320;
-  static const int DEPTH_HEIGHT = 240;
-
-  PXCImage::ColorFormat colorFormat;
 };
 
 int _tmain(int argc, _TCHAR* argv[])
