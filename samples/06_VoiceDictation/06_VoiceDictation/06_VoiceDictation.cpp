ff4c849a076f7299a26d2bb1ad43fa3fc8fa2065
Kaoru NAKAMURA <kaorun55@gmail.com>
2014-02-07 15:45:22 +0900

    音声認識、音声合成のサンプルコードを追加

---
+++ samples/06_VoiceDictation/06_VoiceDictation/06_VoiceDictation.cpp
@@ -0,0 +1,64 @@
+// 06_VoiceDictation.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+#include <Windows.h>
+
+#include "util_pipeline.h"
+
+class Pipeline: public UtilPipeline {
+public:
+
+  // 1.コンストラクタ
+  Pipeline(void)
+    : UtilPipeline()
+  {
+    // 必要なデータを有効にする
+    EnableVoiceRecognition();
+  }
+
+  // 2.音声認識のセットアップを行う
+  virtual void  OnVoiceRecognitionSetup(PXCVoiceRecognition::ProfileInfo * finfo)
+  {
+    // 日本語の音声エンジンを探す
+    auto voiceRecognition = QueryVoiceRecognition();
+    for ( int i = 0; ; ++i ) {
+      PXCVoiceRecognition::ProfileInfo pinfo = { 0 };
+      auto ret = voiceRecognition->QueryProfile( i, &pinfo );
+      if ( ret != PXC_STATUS_NO_ERROR ) {
+        break;
+      }
+
+      if ( pinfo.language == PXCVoiceRecognition::ProfileInfo::LANGUAGE_JP_JAPANESE ) {
+        *finfo = pinfo;
+        std::cout << "日本語の音声エンジンを設定しました" << std::endl;
+      }
+    }
+
+    std::cout << "音声認識を開始します" << std::endl;
+  }
+
+  // 3.文を認識すると呼び出される
+  virtual void PXCAPI OnRecognized( PXCVoiceRecognition::Recognition *cmd ) {
+    std::wcout << L"認識した文: " << cmd->dictation << std::endl;
+  }
+};
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+  try {
+    // 4.wide-charactorを表示できるようにする
+    std::locale::global(std::locale("japanese"));
+
+    Pipeline pipeline;
+    pipeline.LoopFrames();
+  }
+  catch ( std::exception& ex ) {
+    std::cout << ex.what() << std::endl;
+  }
+
+  return 0;
+}
+
---
+++ samples/06_VoiceDictation/08_VoiceSynthesis/08_VoiceSynthesis.cpp
@@ -0,0 +1,70 @@
+// 08_VoiceSynthesis.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+
+#include "util_pipeline.h"
+#include "voice_out.h"
+#pragma comment( lib, "winmm.lib" )
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+  // wide-charactorを扱えるようにする
+  std::locale::global(std::locale("japanese"));
+
+  UtilPipeline pipeline;
+  pipeline.Init();
+
+  PXCVoiceSynthesis* synthesis = nullptr;
+  pipeline.QuerySession()->CreateImpl<PXCVoiceSynthesis>( &synthesis );
+
+  PXCVoiceSynthesis::ProfileInfo pinfo = { 0 };
+  for ( int i = 0; ; ++i ) {
+    auto ret = synthesis->QueryProfile( i, &pinfo );
+    if ( ret != PXC_STATUS_NO_ERROR ) {
+      break;
+    }
+
+    if ( pinfo.language == PXCVoiceRecognition::ProfileInfo::LANGUAGE_JP_JAPANESE ) {
+      synthesis->SetProfile( &pinfo );
+      std::cout << "日本語の音声エンジンを設定しました" << std::endl;
+      break;
+    }
+  }
+
+  VoiceOut voice( &pinfo );
+
+  while ( 1 ) {
+    std::cout << "入力してください：";
+    std::wstring message;
+    std::wcin >> message;
+
+    pxcUID id=0;
+    synthesis->QueueSentence( (wchar_t*)message.c_str(), message.size(), &id );
+
+    for (;;) {
+      PXCSmartSP sp;
+      PXCAudio *sample;
+
+      // Request audio data from TTS
+      auto ret = synthesis->ProcessAudioAsync(id, &sample, &sp);
+      if ( ret<PXC_STATUS_NO_ERROR ) {
+        break;
+      }
+
+      // Make sure there is data that is valid
+      ret = sp->Synchronize();
+      if ( ret<PXC_STATUS_NO_ERROR ) {
+        break;
+      }
+
+      voice.RenderAudio(sample);
+    }
+  }
+
+
+  return 0;
+}
+
